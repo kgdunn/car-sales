57965365e3766c1d63b0fe7a2dd86b77dae88f8e
Kevin Dunn <Kevin.Dunn@dsm.com>
2020-07-08 16:48:24 +0200

    Initial commit.

---
+++ assignment.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+"""
+Assignment goals:
+
+practice creating custom transformers and estimators
+practice performing grid search
+build custom pipelines
+solidify git knowledge and practice working together effectively
+make a first attempt to productionizing ML model
+
+For this lab we will be using car sales dataset from Kaggle. I know - everybody is sick of
+predicting car prices by now, but cars happen to have a good mix of numerical and categorical
+features and that's just what we need for this exercise.
+
+Remember, the main objective of this assignment is not to train the most accurate model, but to
+practice building custom blocks of estimators and transformers.
+
+
+"""
