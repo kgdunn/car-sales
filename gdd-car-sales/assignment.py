c96146c6ff572d3acc0d2fbdc7ec1d8b1f88b50a
Kevin Dunn <Kevin.Dunn@dsm.com>
2020-07-08 18:15:04 +0200

    Solves step 1 of the assignment: to read in the data and split into test/training

--- gdd-car-sales/assignment.py
+++ gdd-car-sales/assignment.py
@@ -1,4 +1,43 @@
+import pathlib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+
+
+def read_data(filename):
+    """
+    Reads the CSV files; calculates features (age) and selects row & column subset
+    """
+    df = pd.read_csv(filename, encoding="ISO-8859-1")
+
+    # Select subset of rows and columns
+    df[(df.loc[:, "seller"] == "privat") & (df.loc[:, "offerType"] == "Angebot")]
+    cols = [
+        "brand",
+        "gearbox",
+        "powerPS",
+        "kilometer",
+        "fuelType",
+        "model",
+        "notRepairedDamage",
+        "yearOfRegistration",
+        "dateCreated",
+    ]
+    df = df[cols]
+
+    # Age column = (date of ad placement: extract only the year) - registration
+    df["age"] = pd.to_datetime(df["dateCreated"]).dt.year - df["yearOfRegistration"]
+    return df
+
+
+if __name__ == "__main__":
+    filename = pathlib.Path.cwd() / "data" / "autos.csv"
+    df = read_data(filename)
+    X_train, X_test = train_test_split(df, test_size=0.40, random_state=42)
+    print(
+        X_train.shape, X_test.shape,
+    )
+    print(X_train.head())
