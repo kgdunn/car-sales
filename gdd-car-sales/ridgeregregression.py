af666e1a3ffc87a7f2262575bb0325328111e22a
Eduard Derks <eduard.derks@dsm.com>
2020-07-08 17:31:23 +0200

    My Ridge Class for sk-learn

--- gdd-car-sales/ridgeregregression.py
+++ gdd-car-sales/ridgeregregression.py
@@ -10,23 +10,19 @@ class MyRidge():
     def __init__(self):
         self.coef_ = None
         
-    def fit(self, X, y, Xt , yt, kappa=1):
+    def fit(self, X, y, lambda=1):
 
         # training data & ground truth data
         self.data = X
         self.target = y
         
-        # degrees of freedom population dep. variable variance 
-        self._dft = X.shape[0] - 1  
-            
-        # Covariance kernels
+        # Compute covariance matrices
         XtX = X.T.dot(X)
-        XtX2 = Xt.T.dot(Xt)
         Xty = X.T.dot(y)
-        Xty2 = Xt.T.dot(yt)
-        
-        # Model Transfer
-        coef = np.linalg.inv(kappa*XtX +(1-kappa)*XtX2).dot(kappa*Xty+(1-kappa)*Xty2)
+        m = X.shape[0]        
+        # Do Rdige regression
+        coef = np.linalg.inv(XtX +lambda*np.eye(m)).dot(Xty)
+
         self.coef_ =coef
 
     def predict(self,X):
