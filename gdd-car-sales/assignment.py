61ed19fcc9469304175609fbb3eebb4fe424e40d
Raaijmakers <Linsey.Raaijmakers@dsm.com>
2020-07-09 16:02:34 +0200

    categorical transformator

--- gdd-car-sales/assignment.py
+++ gdd-car-sales/assignment.py
@@ -4,9 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn.compose import ColumnTransformer
+from sklearn.compose import make_column_transformer
+from sklearn.compose import make_column_selector
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
 
 # Load from the data folder. Note: this script starts in the directory one level higher than the
 # data folder.
@@ -23,13 +27,15 @@ columns_to_use = [
     "dateCreated",
 ]
 
-def convert_to_numerical(df):
-    labelencoder_X = LabelEncoder()
-    df[:,0] = labelencoder_X.fit_transform(X[:,0])
-    onehotencoder = OneHotEncoder(categorical_features=[0,1])
-    X = onehotencoder.fit_transform(X).toarray()
-    
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))])
 
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, selector(dtype_include="category"))])
 
 
 def read_data(filename, columns_to_use):
@@ -37,7 +43,7 @@ def read_data(filename, columns_to_use):
     Reads the CSV files; calculates features (age) and selects row & column subset
     """
     df = pd.read_csv(filename, encoding="ISO-8859-1")
-
+    print(df.dateCreated)
     # Select subset of rows and columns
     df[(df.loc[:, "seller"] == "privat") & (df.loc[:, "offerType"] == "Angebot")]
     # Add only numerical columns
@@ -51,8 +57,10 @@ def read_data(filename, columns_to_use):
 if __name__ == "__main__":
 
     df = read_data(filename, columns_to_use)
-    print(convert_cat_to_num(df))
+    
     X_train, X_test = train_test_split(df, test_size=0.40, random_state=42)
+
+    categorical_transformer()
     print(
         X_train.shape, X_test.shape,
     )
